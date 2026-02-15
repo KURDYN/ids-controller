0e4e602d7d7a1dcdb77251afbb316769e07a51e5
Administrator <kurdynov@gmail.com>
2026-02-15 17:26:06 +0100

    Initial commit

---
+++ src/main/java/com/ids/ids_controller/IdsControllerApplication.java
@@ -0,0 +1,13 @@
+package com.ids.ids_controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdsControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdsControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ids/ids_controller/IdsControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ids.ids_controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdsControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
